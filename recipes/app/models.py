6849abee07a48b6c47f8c29f5b91e81581cad8cf
NikitaKurabtsev <nick3500hunna@gmail.com>
2022-10-27 20:09:25 +0300

    add model and add settings configs

--- recipes/app/models.py
+++ recipes/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    CHOICES = (
+        ('EASY', 'Easy'),
+        ('MEDIUM', 'Medium'),
+        ('HARD', 'Hard')
+    )
+    name = models.CharField(max_length=50, blank=False)
+    description = models.CharField(max_length=255, blank=False)
+    difficult = models.CharField(max_length=10, choices=CHOICES, blank=False)
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.name
---
+++ recipes/app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.all_recipes, name='all_recipes')
+]
